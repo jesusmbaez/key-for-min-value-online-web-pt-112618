6e760cd705df1e642b1f395ee6855ceba3b470f9
Manny Baez <jesusmbaez@gmail.com>
2018-12-15 21:28:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
     lowest_key = nil
   
     name_hash.collect do |key, value|
-      if lowest_value == 0 || value < lowest_key
+      if lowest_value == 0 || value < lowest_value
       
     end 
   end 
