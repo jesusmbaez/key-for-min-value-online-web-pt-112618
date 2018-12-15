c097d317ea9c4cb6f712562f19e6fbc2909c9683
Manny Baez <jesusmbaez@gmail.com>
2018-12-15 21:41:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -16,9 +16,9 @@ end
 
 # We need a baseline value to compare against the value in our hash. Set the value to 0 in a variable. We need to do the same for our key. Set your lowest_key to nil so it returns nill if a hash is not passed in as an argument 
 
-# iterate over the hash with collect (stores in memory). To iterate over hashes you need to pass a black ofthe key and the value 
+# Iterate over the hash with collect (stores in memory). To iterate over hashes you need to pass a block. For hashes that means a key and a value within that key.  
 
-# use if statement to compare. if your lowest value is equal to 0 OR your value is less than your lowest_value then lowest_value is equals value and lowest key is key 
+# Use if statement to control the folow and to to compare items within the hash. If your lowest value is equal to 0 (you set this to 0 in your variable and will what is used for the first iteration) OR value passed in is less than your lowest_value previously passed in then lowest_value is now equals value (the value passed in during iteration) and lowest key is now key (the key passed in during iteration).  
 
 # return lowest_key
 
